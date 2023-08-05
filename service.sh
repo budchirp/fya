ed9324a2fcef687551cb38e380f0d4993450664d
budchirp <budchirp@gmail.com>
2023-08-05 21:13:13 +0300

    fix: increase watcher limit

--- service.sh
+++ service.sh
@@ -2,13 +2,11 @@
 
 MODDIR=${0%/*}
 
-{
-	until [[ "$(getprop sys.boot_completed)" == "1" ]]; do
-		sleep 1
-	done
+until [[ "$(getprop sys.boot_completed)" == "1" ]]; do
+	sleep 1
+done
 
-	device_config put activity_manager max_phantom_processes 2147483647
-	resetprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
+device_config put activity_manager max_phantom_processes 2147483647
+resetprop persist.sys.fflag.override.settings_enable_monitor_phantom_procs false
 
-	echo 524288 >/proc/sys/fs/inotify/max_user_watches
-} &
+sysctl -w fs.inotify.max_user_watches=2147483647
